c477937f0cd28a30ac11c2d6536a15ee39cdf705
Thaler <Roman.Thaler@adesso.at>
2019-08-21 14:04:19 +0200

    TESTING - another integration test

--- src/app/anagramm-listing/anagramm-listing.component.spec.ts
+++ src/app/anagramm-listing/anagramm-listing.component.spec.ts
@@ -1,36 +1,40 @@
-import { Component } from '@angular/core';
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { By } from '@angular/platform-browser';
-import { BehaviorSubject, of, throwError } from 'rxjs';
-import { DictonaryService } from '../dictonary.service';
-import { NoDupsPipe } from '../no-dups.pipe';
-import { AnagrammListingComponent } from './anagramm-listing.component';
-
+import { Component } from "@angular/core";
+import { async, ComponentFixture, TestBed } from "@angular/core/testing";
+import { By } from "@angular/platform-browser";
+import { BehaviorSubject, of, throwError } from "rxjs";
+import { DictonaryService } from "../dictonary.service";
+import { NoDupsPipe } from "../no-dups.pipe";
+import { AnagrammListingComponent } from "./anagramm-listing.component";
 
 @Component({
-  template: `<app-anagramm-listing [anaCandidate$]="myString$"></app-anagramm-listing>`,
+  template: `
+    <app-anagramm-listing [anaCandidate$]="myString$"></app-anagramm-listing>
+  `
 })
 export class HostComponent {
-  public myString$ = new BehaviorSubject('alpen');
+  public myString$ = new BehaviorSubject("alpen");
 }
 
-describe('AnagrammListingComponent', () => {
+describe("AnagrammListingComponent", () => {
   let component: HostComponent;
   let fixture: ComponentFixture<HostComponent>;
+  let service: DictonaryService;
+  service = TestBed.get(DictonaryService);
 
   let dictonaryMock: jasmine.SpyObj<DictonaryService>;
 
   beforeEach(async(() => {
-    dictonaryMock = jasmine.createSpyObj('DictonaryService', ['getWordsByFirstLetter$']);
-    dictonaryMock.getWordsByFirstLetter$.and.returnValue(of(['alpen', 'napel', 'lapen']));
+    dictonaryMock = jasmine.createSpyObj("DictonaryService", [
+      "getWordsByFirstLetter$"
+    ]);
+    dictonaryMock.getWordsByFirstLetter$.and.returnValue(
+      of(["alpen", "napel", "lapen"])
+    );
 
     TestBed.configureTestingModule({
-      declarations: [ HostComponent, AnagrammListingComponent, NoDupsPipe ],
-      providers: [
-        { provide: DictonaryService, useValue: dictonaryMock },
-      ],
-    })
-    .compileComponents();
+      declarations: [HostComponent, AnagrammListingComponent, NoDupsPipe],
+      providers: [{ provide: DictonaryService, useValue: dictonaryMock }]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
@@ -39,36 +43,42 @@ describe('AnagrammListingComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', async(() => {
+  it("should create", async(() => {
     expect(component).toBeTruthy();
   }));
 
-  it('should propagate any errors', async(() => {
+  it("should propagate any errors", async(() => {
     // Given
-    dictonaryMock.getWordsByFirstLetter$.and.returnValue(throwError(new Error('Just a mock')));
-    spyOn(console, 'warn').and.callFake(() => {
+    dictonaryMock.getWordsByFirstLetter$.and.returnValue(
+      throwError(new Error("Just a mock"))
+    );
+    spyOn(console, "warn").and.callFake(() => {
       expect(console.warn).toHaveBeenCalled();
     });
 
     // When
     //component.myString$.next('This will fail');
 
-
     // Then
   }));
 
-  fit('should render list of anagrams in li tags', () => {
+  it("should render list of anagrams in li tags", () => {
     // Given
-    const listingComponent: AnagrammListingComponent = fixture.debugElement.query(By.css('li')).componentInstance;
-    listingComponent.anagrams = [{ value: 'a', color: 'colored' }, { value: 'b', color: 'colored' }, { value: 'c', color: 'colored' }];
+    const listingComponent: AnagrammListingComponent = fixture.debugElement.query(
+      By.css("li")
+    ).componentInstance;
+    listingComponent.anagrams = [
+      { value: "a", color: "colored" },
+      { value: "b", color: "colored" },
+      { value: "c", color: "colored" }
+    ];
 
     // When
     fixture.detectChanges();
 
     // Then
-    console.log(fixture.nativeElement.querySelector('li'));
-    expect(fixture.nativeElement.querySelector('li')).toBeTruthy();
+    expect(fixture.nativeElement.querySelector("li").textContent).toContain(
+      "a"
+    );
   });
-
-  it('should highlight anagram if it is a dictionary entry');
 });
